a12a1d1b7e25ce1f9668184eda50bb6159b32e89
Drum-J <tmdgh717@naver.com>
2024-02-06 15:13:59 +0900

    feat. 엔티티 생성/수정 감지를 위한 애노테이션 추가

--- src/main/java/com/jaws/JawsApplication.java
+++ src/main/java/com/jaws/JawsApplication.java
@@ -2,8 +2,10 @@ package com.jaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JawsApplication {
 
     public static void main(String[] args) {
