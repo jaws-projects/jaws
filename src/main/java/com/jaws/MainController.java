52d929738ac0ae12a97f63a2a78a6729f273e0aa
hyn.xp <50633008+kyunghyun-Park@users.noreply.github.com>
2024-02-14 13:43:37 +0900

    Merge pull request #5
    * feat: 타임리프 설정파일 및 의존성 추가
    
    * feat: 타임리프 파일 경로 수정
    
    * feat: 타임리프 초기 파일 커밋
    
    * chore: 타임리프 경로 수정
    
    * chore: 자동반영, 타임리프 레이아웃 의존성 추가
    
    * feat: 초기 레이아웃 생성 및 분할
    
    * design: 로고, 아이콘 이미지 추가
    
    * design: 공통 css 추가(추후 복잡할 시 분할 예정)
    
    * feat: 초기 화면 확인을 위한 매핑 추가
    
    * design: 부트스트랩4 cdn 제거

---
+++ src/main/java/com/jaws/MainController.java
@@ -0,0 +1,17 @@
+package com.jaws;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String main() {
+        log.info("main controller");
+        return "contents/main";
+    }
+
+}
